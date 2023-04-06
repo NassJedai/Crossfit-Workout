668a381576d95943789b1091e095fde2841528fb
Nassjedai <jedainassim@gmail.com>
2023-04-06 13:32:27 +0200

    crossfit workout generator

--- pages/wod.js
+++ pages/wod.js
@@ -3,23 +3,25 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Wod() {
-  const [program, setProgram] = useState("conditioning");
+  const [programming, setProgramming] = useState("conditioning");
   const [result, setResult] = useState("");
-
-  const handleChange = (event) => {
-    setProgram(event.target.value);
-    };
-
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+
+    if(loading) {
+      return;
+    }
+
+    setLoading(true);
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("/api/generate-wod", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ program: program }),
+        body: JSON.stringify({ programming }),
       });
 
       const data = await response.json();
@@ -27,11 +29,13 @@ export default function Wod() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
+      setResult(data.result.replaceAll('\\n', '<br />'));
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -47,15 +51,24 @@ export default function Wod() {
         <h3>Crossfit workout generator</h3>
 
         <form onSubmit={onSubmit}>
-        <select value={program} onChange={handleChange}>
-            <option value="conditioning">Conditioning</option>
-            <option value="gymnastics">Gymnastics</option>
-            <option value="weightlifting">Weightlifting</option>
-        </select>
-          <input type="submit" value="Generate Workout" />
+          <select value={programming} onChange={(e) => setProgramming(e.target.value)}>
+              <option value="conditioning">Conditioning</option>
+              <option value="gymnastics">Gymnastics</option>
+              <option value="weightlifting">Weightlifting</option>
+          </select>
+            <input type="submit" value="Generate Workout" />
         </form>
 
-        <div className={styles.result}>{result}</div>
+        {loading && (
+          <div>
+            <h3>Looking for the best gift ideas 🎁 💡</h3>
+            <img src="/loading.gif" className={styles.loading} />
+          </div>
+        )}
+        <div
+          className={styles.result}
+          dangerouslySetInnerHTML={{ __html: result }}
+        />
       </main>
     </div>
   );
